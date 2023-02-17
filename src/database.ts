35281ececf8113debc66490f56c7d30ed7375445
Tales Eduardo <tales.silva@psvsistemas.com.br>
2023-02-17 11:11:01 -0300

    mais configurações para deploy

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,18 @@
 import { knex as stupKnex, Knex } from 'knex';
 import { env } from './env';
 
+const sqliteConnection = {
+  filename: env.DATABASE_URL,
+}
+
+const postgreConnection = {
+  filename: env.DATABASE_URL,
+
+}
+
 export const config : Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL
-  },
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_CLIENT === 'sqlite'? sqliteConnection : postgreConnection,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
